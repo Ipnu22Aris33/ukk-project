89baf906e08b8af313808c6a4a11318750c2f429
ipnu aris saputra <ipnuarissaputra@gmail.com>
2026-04-20 04:59:04 +0700

    refactor: standardize layout styles in ActivatePage and LoginPage components

--- src/app/auth/activate/page.tsx
+++ src/app/auth/activate/page.tsx
@@ -3,8 +3,21 @@ import ActivateForm from './_components/ActivateForm';
 
 export default function ActivatePage() {
   return (
-    <Flex align='center' justify='center' style={{ minHeight: '100vh', padding: '16px' }}>
-      <Container size='1'>
+    <Flex
+      align="center"
+      justify="center"
+      style={{
+        minHeight: '100dvh',
+        padding: '16px',
+        boxSizing: 'border-box',
+      }}
+    >
+      <Container
+        size="1"
+        style={{
+          width: '100%',
+        }}
+      >
         <Box>
           <ActivateForm />
         </Box>
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,8 +3,21 @@ import LoginForm from './_components/LoginForm';
 
 export default function LoginPage() {
   return (
-    <Flex align='center' justify='center' style={{ minHeight: '100vh', padding: '16px' }}>
-      <Container size='1'>
+    <Flex
+      align='center'
+      justify='center'
+      style={{
+        minHeight: '100dvh',
+        padding: '16px',
+        boxSizing: 'border-box',
+      }}
+    >
+      <Container
+        size='1'
+        style={{
+          width: '100%',
+        }}
+      >
         <Box>
           <LoginForm />
         </Box>
